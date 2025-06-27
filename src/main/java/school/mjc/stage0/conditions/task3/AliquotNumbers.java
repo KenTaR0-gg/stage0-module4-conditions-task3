2a8d33679d56679daf8d764d59891dbb2740af3c
KenTaR0-gg <kotya.piso@gmail.com>
2025-06-27 03:45:05 +0300

    module 4. task 3

--- src/main/java/school/mjc/stage0/conditions/task3/AliquotNumbers.java
+++ src/main/java/school/mjc/stage0/conditions/task3/AliquotNumbers.java
@@ -2,5 +2,11 @@ package school.mjc.stage0.conditions.task3;
 
 public class AliquotNumbers {
     public void isFirstAliquot(int first, int second) {
+        if (first>0 && second>first){
+            System.out.println("Aliquot");
+        }
+        else {
+            System.out.println("Not aliquot");
+        }
     }
 }
--- src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
+++ src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
@@ -2,5 +2,35 @@ package school.mjc.stage0.conditions.task3;
 
 public class AlphabeticCharacters {
     public void vowelDeterminer(char character) {
+        // если не бет рабоать попробовать через палки и в один иф
+        /*
+        if ((character > 65) && (character < 69))
+            if ((character > 69) && (character < 73))
+                if ((character > 73) && (character < 79))
+                   if ((character > 79) && (character < 85))
+                       if ((character > 85) && (character < 89))
+                           if (character == 90)
+                               if ((character > 97) && (character < 101))
+                                   if ((character > 101) && (character < 105))
+                                       if ((character > 105) && (character < 111))
+                                           if ((character > 111) && (character < 117))
+                                               if ((character > 117) && (character < 121))
+                                                   if (character == 122) {
+                                                       System.out.println("Vowel");
+                                                   } else if (character == 65 || character == 69 || character == 73 || character == 79 || character == 85 || character == 89 || character == 97 || character == 101 || character == 105 || character == 111 || character == 117 || character == 121) {
+                                                       System.out.println("Consonant");
+                                                   } else {
+                                                       System.out.println("wrong alphabet!");
+                                                   }
+
+         */
+        if (character == 'e' ||character == 'y' ||character == 'u' ||character == 'o' ||character == 'a' ||character == 'i') {
+            System.out.println("Vowel");
+        } else if (character == 'B') {
+            System.out.println("Consonant");
+        } else {
+            System.out.println("wrong alphabet!");
+        }
+
     }
 }
